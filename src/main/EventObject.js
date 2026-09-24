3c000e044c759307570814b8448c5398424cf7a2
Winstonshih <Winstonshih@github.com>
2026-09-24 01:38:54 +0000

    Test run: 0/4 passed, 4 failed, 0 skipped - 2026-09-24T01:38:54.772Z

--- src/main/EventObject.js
+++ src/main/EventObject.js
@@ -22,7 +22,7 @@ let content = document.getElementById("content");
  * some information about the clicked element will be displayed.
  */
 function updateContentWithEventObjectTarget(eventObject){
-    content.innertext=eventObject
+    content.innerText=eventObject
 }
 
 
